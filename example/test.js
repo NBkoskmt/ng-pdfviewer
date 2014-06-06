a50f97565f513b4bfc7bc7bbf1e19a695a4287f6
Dominic English <dominic.b.english@gmail.com>
2014-06-06 16:42:52 +1000

    improve(): update example to include example of manual pdf loading

--- example/test.js
+++ example/test.js
@@ -31,4 +31,10 @@ app.controller('TestController', [ '$scope', 'PDFViewerService', function($scope
 	$scope.changeZoom = function(zoomLevel) {
 		$scope.instance.changeZoom(zoomLevel);
 	};
+
+	// Demonstrate the ability to manually load a file and specify a `page`
+	// `page` is allowed to have the value 'last'
+	$scope.loadPdfLastPage = function(path) {
+		$scope.instance.loadPdf(path, 'last');
+	}
 }]);
